b62c773ffb85987aa953aaddc485adcb5a6da4d8
cldpk <deepak14cl@gmail.com>
2022-08-16 18:32:26 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,96 @@
+  provider "aws"{
+    region = "us-east-1"
+    access_key = "AKIAW7MXVADCZWZEFA7I"
+  secret_key = "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"
+}
+
+# Query all avilable Availibility Zone
+data "aws_availability_zones" "available" {}
+
+# VPC Creation
+
+resource "aws_vpc" "main" {
+  cidr_block           = "${var.vpc_cidr}"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "my-new-test-terraform-vpc"
+  }
+}
+
+# Creating Internet Gateway
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  tags = {
+    Name = "my-test-igw"
+  }
+}
+# Public Route Table
+
+resource "aws_route_table" "public_route" {
+  vpc_id = "${aws_vpc.main.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+
+  tags = {
+    Name = "my-test-public-route"
+  }
+}
+
+# Private Route Table
+
+resource "aws_default_route_table" "private_route" {
+  default_route_table_id = "${aws_vpc.main.default_route_table_id}"
+
+
+  tags = {
+    Name = "my-private-route-table"
+  }
+}
+# Public Subnet
+resource "aws_subnet" "public_subnet" {
+  count                   = 2
+  cidr_block              = "${var.public_cidrs[count.index]}"
+  vpc_id                  = "${aws_vpc.main.id}"
+  map_public_ip_on_launch = true
+  availability_zone       = "${data.aws_availability_zones.available.names[count.index]}"
+
+  tags = {
+    Name = "my-test-public-subnet.${count.index + 1}"
+  }
+}
+
+# Private Subnet
+resource "aws_subnet" "private_subnet" {
+  count             = 2
+  cidr_block        = "${var.private_cidrs[count.index]}"
+  vpc_id            = "${aws_vpc.main.id}"
+  availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
+
+  tags = {
+    Name = "my-test-private-subnet.${count.index + 1}"
+  }
+}
+# Associate Public Subnet with Public Route Table
+resource "aws_route_table_association" "public_subnet_assoc" {
+  count          = 2
+  route_table_id = "${aws_route_table.public_route.id}"
+  subnet_id      = "${aws_subnet.public_subnet.*.id[count.index]}"
+  depends_on     = ["aws_route_table.public_route", "aws_subnet.public_subnet"]
+}
+
+# Associate Private Subnet with Private Route Table
+resource "aws_route_table_association" "private_subnet_assoc" {
+  count          = 2
+  route_table_id = "${aws_default_route_table.private_route.id}"
+  subnet_id      = "${aws_subnet.private_subnet.*.id[count.index]}"
+  depends_on     = ["aws_default_route_table.private_route", "aws_subnet.private_subnet"]
+}
+
+
